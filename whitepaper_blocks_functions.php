5ec90c6819f09f1c1a7df5a3086df85e5e003997
Dillon Brickhouse <trombonecricket@gmail.com>
2019-03-23 13:42:12 -0600

    Switched dashes to underscores for consistency

--- whitepaper_blocks_functions.php
+++ whitepaper_blocks_functions.php
@@ -77,22 +77,22 @@ function whitepaper_parse_handlebars( array $matches = array() ) {
 
 			/***** The following handlebars are built to work with the Events Organiser plugin *****/
 			// Get the event start date
-			case 'event-start-date':
+			case 'event_start_date':
 				return eo_get_the_start("M j, Y");
 				break;
 
 			// Get the event end date
-			case 'event-end-date':
+			case 'event_end_date':
 				return eo_get_the_end("M j, Y");
 				break;
 
 			// Get the event start time
-			case 'event-start-time':
+			case 'event_start_time':
 				return eo_get_the_start("g:i A");
 				break;
 
 			// Get the event end time
-			case 'event-end-time':
+			case 'event_end_time':
 				return eo_get_the_end("g:i A");
 				break;
 
