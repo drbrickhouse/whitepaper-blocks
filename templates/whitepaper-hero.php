7c233b6b9b2ce78cd54e3daeb06a14cbf38dd101
Dillon Brickhouse <trombonecricket@gmail.com>
2019-03-24 21:30:47 -0600

    Updated hero template to make it easier to style

--- templates/whitepaper-hero.php
+++ templates/whitepaper-hero.php
@@ -12,12 +12,12 @@ $hero_overlay = get_field( 'hero_overlay' );
 //Hero Layout
 ?>
 
-<div class="row whitepaper-block" id="<?php echo $block_id ?>">
+<div class="row whitepaper-block" id="<?php echo $block_id ?>" style="background-image: url('<?php echo $background_image; ?>');">
   <div class="col-md-12">
     <div class="row whitepaper-block-inner">
       <div class="col-md-12">
-        <div class="<?php echo $hero_class; ?>" style="background-image: url('<?php echo $background_image; ?>');">
-          <div class="col-md-12 hero-overlay">
+        <div class="<?php echo $hero_class; ?> hero-overlay">
+          <div class="col-md-12">
             <?php echo $hero_overlay; ?>
           </div>
         </div>
